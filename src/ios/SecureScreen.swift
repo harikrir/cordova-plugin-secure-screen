e0dd1b16f3f667c6d47c5f541ecb3dc84ae173e3
harikrir <hkrishnan254@gmail.com>
2026-08-03 09:32:42 +0300

    Update SecureScreen.swift

--- src/ios/SecureScreen.swift
+++ src/ios/SecureScreen.swift
@@ -1,2 +1,108 @@
 import UIKit
-@objc(SecureScreen) class SecureScreen: CDVPlugin { var blurView:UIVisualEffectView?; @objc(enableScreenshotProtection:) func enableScreenshotProtection(command:CDVInvokedUrlCommand){let r=CDVPluginResult(status:CDVCommandStatus_OK);commandDelegate.send(r, callbackId: command.callbackId)} @objc(disableScreenshotProtection:) func disableScreenshotProtection(command:CDVInvokedUrlCommand){let r=CDVPluginResult(status:CDVCommandStatus_OK);commandDelegate.send(r, callbackId: command.callbackId)} @objc(enableAppSwitcherBlur:) func enableAppSwitcherBlur(command:CDVInvokedUrlCommand){NotificationCenter.default.addObserver(self, selector:#selector(addBlur), name:UIApplication.willResignActiveNotification, object:nil);NotificationCenter.default.addObserver(self, selector:#selector(removeBlur), name:UIApplication.didBecomeActiveNotification, object:nil);let r=CDVPluginResult(status:CDVCommandStatus_OK);commandDelegate.send(r, callbackId: command.callbackId)} @objc(disableAppSwitcherBlur:) func disableAppSwitcherBlur(command:CDVInvokedUrlCommand){NotificationCenter.default.removeObserver(self);let r=CDVPluginResult(status:CDVCommandStatus_OK);commandDelegate.send(r, callbackId: command.callbackId)} @objc func addBlur(){guard blurView == nil else{return}; let blur=UIVisualEffectView(effect:UIBlurEffect(style:.light)); blur.frame=UIScreen.main.bounds; UIApplication.shared.windows.first?.addSubview(blur); blurView=blur} @objc func removeBlur(){blurView?.removeFromSuperview(); blurView=nil}}
+
+@objc(SecureScreen)
+class SecureScreen: CDVPlugin {
+
+    private var blurView: UIVisualEffectView?
+
+    @objc(enableScreenshotProtection:)
+    func enableScreenshotProtection(command: CDVInvokedUrlCommand) {
+
+        let result = CDVPluginResult(
+            status: CDVCommandStatus_OK
+        )
+
+        commandDelegate.send(
+            result,
+            callbackId: command.callbackId
+        )
+    }
+
+    @objc(disableScreenshotProtection:)
+    func disableScreenshotProtection(command: CDVInvokedUrlCommand) {
+
+        let result = CDVPluginResult(
+            status: CDVCommandStatus_OK
+        )
+
+        commandDelegate.send(
+            result,
+            callbackId: command.callbackId
+        )
+    }
+
+    @objc(enableAppSwitcherBlur:)
+    func enableAppSwitcherBlur(command: CDVInvokedUrlCommand) {
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(addBlur),
+            name: UIApplication.willResignActiveNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(removeBlur),
+            name: UIApplication.didBecomeActiveNotification,
+            object: nil
+        )
+
+        let result = CDVPluginResult(
+            status: CDVCommandStatus_OK
+        )
+
+        commandDelegate.send(
+            result,
+            callbackId: command.callbackId
+        )
+    }
+
+    @objc(disableAppSwitcherBlur:)
+    func disableAppSwitcherBlur(command: CDVInvokedUrlCommand) {
+
+        NotificationCenter.default.removeObserver(self)
+
+        let result = CDVPluginResult(
+            status: CDVCommandStatus_OK
+        )
+
+        commandDelegate.send(
+            result,
+            callbackId: command.callbackId
+        )
+    }
+
+    @objc
+    private func addBlur() {
+
+        guard blurView == nil else {
+            return
+        }
+
+        let blurEffect = UIBlurEffect(style: .light)
+
+        let blur = UIVisualEffectView(
+            effect: blurEffect
+        )
+
+        blur.frame = UIScreen.main.bounds
+        blur.autoresizingMask = [
+            .flexibleWidth,
+            .flexibleHeight
+        ]
+
+        UIApplication.shared.windows.first?.addSubview(
+            blur
+        )
+
+        blurView = blur
+    }
+
+    @objc
+    private func removeBlur() {
+
+        blurView?.removeFromSuperview()
+        blurView = nil
+    }
+}
